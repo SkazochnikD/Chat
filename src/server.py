28ff3f0f2921970d2b0779aea18281938b1dbb73
unknown <tdv-mb@mail.ru>
2020-04-08 09:26:10 +0400

    Add project

---
+++ src/server.py
@@ -0,0 +1,93 @@
+#
+# Серверное приложение для соединений
+#
+import asyncio
+from asyncio import transports
+
+
+class ServerProtocol(asyncio.Protocol):
+    login: str = None
+    server: 'Server'
+    transport: transports.Transport
+
+    def __init__(self, server: 'Server'):
+        self.server = server
+
+    def send_history(self):
+        for message in self.server.history:
+            self.transport.write(message)
+
+    def data_received(self, data: bytes):
+        print(data)
+
+        decoded = data.decode()
+
+        if self.login is not None:
+            self.send_message(decoded)
+        else:
+            if decoded.startswith("login:"):
+                user_login: str = decoded.replace("login:", "").replace("\r\n", "")
+                wrong_login = False
+                for user in self.server.clients:
+                    if user.login == user_login:
+                        self.transport.write("Пользователь с таким логином уже зарегистрирован\n".encode())
+                        wrong_login=True
+                        break
+                if wrong_login == False:
+                    self.login = decoded.replace("login:", "").replace("\r\n", "")
+                    self.transport.write(
+                        f"Привет, {self.login}!\n".encode()
+                    )
+                    self.send_history()
+            else:
+                self.transport.write("Неправильный логин\n".encode())
+
+    def connection_made(self, transport: transports.Transport):
+        self.server.clients.append(self)
+        self.transport = transport
+        print("Пришел новый клиент")
+
+    def connection_lost(self, exception):
+        self.server.clients.remove(self)
+        print("Клиент вышел")
+
+    def send_message(self, content: str):
+        message = f"{self.login}: {content}\n"
+
+        for user in self.server.clients:
+            user.transport.write(message.encode())
+            if len(self.server.history) > 9:
+                del self.server.history[0]
+            self.server.history.append(message.encode())
+
+class Server:
+    clients: list
+    history: list
+
+    def __init__(self):
+        self.clients = []
+        self.history = []
+
+    def build_protocol(self):
+        return ServerProtocol(self)
+
+    async def start(self):
+        loop = asyncio.get_running_loop()
+
+        coroutine = await loop.create_server(
+            self.build_protocol,
+            '127.0.0.1',
+            8888
+        )
+
+        print("Сервер запущен ...")
+
+        await coroutine.serve_forever()
+
+
+process = Server()
+
+try:
+    asyncio.run(process.start())
+except KeyboardInterrupt:
+    print("Сервер остановлен вручную")
